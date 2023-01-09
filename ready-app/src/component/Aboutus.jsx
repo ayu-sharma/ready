326cf1540f79fa96dc92e8fb148837a7cba3ea45
AkshatJha21 <akshatj2134@gmail.com>
2023-01-09 20:38:14 +0530

    footer updated + css changes

--- ready-app/src/component/Aboutus.jsx
+++ ready-app/src/component/Aboutus.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Aboutus() {
   return (
-    <div className='container drop-shadow bg-white mx-auto mt-14 mb-24 align-center w-10/12 md:flow-root rounded-lg flex md:block flex-col items-center justify-center '>
+    <div className='container drop-shadow bg-white mx-auto mt-14 mb-24 align-center w-10/12 md:flow-root rounded-lg flex md:block flex-col items-center justify-center border-2 border-slate-200'>
       <p className='mx-10 py-5 font-semibold text-xl'>About Us</p>
       <div className='mb-10 mx-10 md:float-left md:flex'>
         <p className='md:w-6/12 text-base md:mr-10'>Ready is an initiative to bring brick-and-mortar retail stores onto the internet and help them become a part of the e-commerce industry. We, at Ready, value time above everything else. Hence, it is our mission to provide fast services to both, our business partners and our customers.</p>
