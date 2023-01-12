39ab9faa049ed0628f8feef08823ba44df93faf3
AkshatJha21 <akshatj2134@gmail.com>
2023-01-12 20:06:09 +0530

    route using props

--- ready-app/src/App.js
+++ ready-app/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Sellersignuppage from './component/Sellersignuppage';
-import Buyersignuppage from './component/Buyersignuppage';
+import Signuppage from './component/Signuppage';
 import Home from './component/Home';
 import Mainnavbar from './component/Mainnavbar';
-import Buyerloginpage from './component/Buyerloginpage';
-import Sellerloginpage from './component/Sellerloginpage';
+import Loginpage from './component/Loginpage';
 import Registerstore from './component/Registerstore';
 import Aboutus from './component/Aboutus';
 import Footer from './component/Footer';
@@ -24,23 +22,18 @@ function App() {
     <div>
       <Mainnavbar />
       
-      {/* <Home /> */}
-      {/* <Signuppage /> */}
-      {/* <Loginpage /> */}
-      {/* <Registerstore /> */}
-      {/* <Aboutus/> */}
       <Routes>
           <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/seller-signup" element={<Signuppage cta="You are one step closer to setting up your online store! Sign up and get started." button="/get-started" link="/seller-login"/>}/>
+          <Route exact path="/buyer-signup" element={<Signuppage cta="Sign up and start shopping from your favourite stores in town." button="/location" link="/buyer-login"/>}/>
+          <Route exact path="/buyer-login" element={<Loginpage cta="Sign in and continue your shopping." button="/home" link="/buyer-signup"/>}/>
+          <Route exact path="/seller-login" element={<Loginpage cta="Sign in and start growing your online store." button="/store-dashboard" link="/seller-signup"/>}/>
           <Route exact path="/get-started" element={<Getstarted/>}/>
-          <Route exact path="/store-dashboard" element={<Storedashboard/>}/>
-          <Route exact path="/seller-signup" element={<Sellersignuppage/>}/>
-          <Route exact path="/buyer-signup" element={<Buyersignuppage/>}/>
           <Route exact path="/register-store" element={<Registerstore/>}/>
-          <Route exact path="/about" element={<Aboutus/>}/>
-          <Route exact path="/buyer-login" element={<Buyerloginpage/>}/>
-          <Route exact path="/seller-login" element={<Sellerloginpage/>}/>
           <Route exact path="/location" element={<Location/>}/>
+          <Route exact path="/store-dashboard" element={<Storedashboard/>}/>
           <Route exact path="/payment-info" element={<Paymentinfo/>}/>
+          <Route exact path="/about" element={<Aboutus/>}/>
       </Routes> 
       <Footer/>
     </div>
